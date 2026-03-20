3ad621318370cda35d63c1e062bc59cd783e82fb
Archit Verma <thearchit02@gmail.com>
2026-03-21 03:21:04 +0530

    Add Spring Boot configuration with PostgreSQL

---
+++ src/main/java/com/bimigration/BiMigrationToolApplication.java
@@ -0,0 +1,13 @@
+package com.bimigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiMigrationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiMigrationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bimigration/BiMigrationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bimigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiMigrationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
